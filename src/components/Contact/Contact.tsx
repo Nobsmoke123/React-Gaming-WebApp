3ba19f76d3b1f903ec01c06623d008d01d047d84
Nobsmoke123 <donaldakobundu@gmail.com>
2025-03-10 00:49:02 +0100

    add Footer component with social media links and enhance Contact section with new layout and button

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,3 +1,4 @@
+import Button from "../Button/Button";
 import ImageClipBox from "../ImageClipBox/ImageClipBox";
 
 const Contact = () => {
@@ -15,7 +16,29 @@ const Contact = () => {
             clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
           />
         </div>
-        <div className="absolute left-20 -top-40 hidden w-60 "></div>
+        <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
+          <ImageClipBox
+            src="img/swordman-partial.webp"
+            clipClass="absolute md:scale-125"
+          />
+          <ImageClipBox
+            src="img/swordman.webp"
+            clipClass="sword-man-clip-path md:scale-125"
+          />
+        </div>
+
+        <div className="flex flex-col items-center text-center">
+          <p className="font-general text-[10px] uppercase">Join Zentry</p>
+          <p className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]">
+            Let's b<b>u</b>ild the <br /> n<b>e</b>w era of <br /> g<b>a</b>ming
+            t<b>o</b>gether
+          </p>
+          <Button
+            title="contact us"
+            containerClass="mt-10 cursor-pointer"
+            id="contact-us"
+          />
+        </div>
       </div>
     </div>
   );
